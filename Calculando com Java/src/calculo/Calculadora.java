4eed0724aba28f8b028a77fce8db28461536b2cc
GTreccia <51425669+GTreccia@users.noreply.github.com>
2019-07-12 23:12:11 -0300

    Add files via upload

---
+++ Calculando com Java/src/calculo/Calculadora.java
@@ -0,0 +1,69 @@
+package calculo;
+import java.util.Scanner;
+public class Calculadora {
+	public static void main(String[] args) {
+		System.out.println("Feito por GTreccia.");
+		System.out.println("ATENÇÃO: PROGRAMA INSTÁVEL, SIGA CORRETAMENTE O QUE SE PEDE.");
+		System.out.println("Qualquer coisa diferente o programa se encerra.");
+		System.out.println("Calculadora Java");
+		System.out.println("Insira 1 para somar;");
+		System.out.println("Insira 2 para subtrair;");
+		System.out.println("Insira 3 para multiplicar;");
+		System.out.println("Insira 4 para dividir.");
+		System.out.print("Opcao: ");
+		Scanner in = new Scanner ( System.in );
+		int dec = in.nextInt();
+		double num1, num2, num3;
+		System.out.println("|------"+ dec+ "------|");
+			
+	
+		if(dec == 1) {
+			System.out.println("Insira o primeiro numero: ");
+			num1 = in.nextDouble();
+			System.out.println("Insira o segundo numero: ");
+			num2 = in.nextDouble();
+			
+			num3 = num1 + num2;
+			
+			System.out.println("O resultado e: "+ num3);
+		}else { if(dec == 2) {
+			System.out.println("Insira o primeiro numero: ");
+			num1 = in.nextDouble();
+			System.out.println("Insira o segundo numero: ");
+			num2 = in.nextDouble();
+			
+			num3 = num1 - num2;
+			
+			System.out.println("O resultado e: "+ num3);
+		} else {
+			if(dec == 3) {
+		
+			System.out.println("Insira o primeiro numero: ");
+			num1 = in.nextDouble();
+			System.out.println("Insira o segundo numero: ");
+			num2 = in.nextDouble();
+			
+			num3 = num1 * num2;
+			
+			System.out.println("O resultado e: "+ num3);
+	
+		} else { if(dec == 4) {
+			System.out.println("Insira o primeiro numero: ");
+			num1 = in.nextDouble();
+			System.out.println("Insira o segundo numero: ");
+			num2 = in.nextDouble();
+			
+			num3 = num1 / num2;
+			
+			System.out.println("O resultado e: "+ num3);
+	
+		} else {
+			System.out.println("Valor invalido");
+			}
+		   }
+		  }
+		 }
+	    }
+       }
+
+
